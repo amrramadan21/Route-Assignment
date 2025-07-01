d9d96fd16f68336a47496c2b5772fe3cd8bb7500
amr <amrr.am7577@gamil.com>
2025-07-01 07:14:45 +0300

    Solution Of Assignment 3

---
+++ Assignment 01/Program.cs
@@ -0,0 +1,155 @@
+﻿namespace Assignment_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Assignment
+
+
+            #region Q1
+
+            //Console.WriteLine("Enter A Number: ");
+            //int Number = int.Parse(Console.ReadLine()) ;
+
+            //Console.WriteLine("Your entered: " + Number);
+
+            #endregion
+
+
+            #region Q2
+
+            //string str = "amr123";
+            //int num = int.Parse(str);
+            //Console.WriteLine(num);
+
+            // the input string "amr123" was not in a correct format
+
+
+
+            #endregion
+
+
+            #region Q3
+
+            //double Number1 =5.4 ;
+            //double Number2 =4.6;
+
+            //Console.WriteLine($"Sum = {Number1 + Number2}");//10
+            //Console.WriteLine($"Mitiply = {Number1 * Number2}");//24.84
+            //Console.WriteLine($"Subtract = {Number1 - Number2}");//0.8
+            //Console.WriteLine($"Division = {Number1 / Number2}");//1.17
+
+            //Print the numbers in decimal 
+
+
+            #endregion
+
+
+            #region Q4
+
+            //string fullName = "Amr Ramadan";
+            //string subName = fullName.Substring(4, 7);
+
+            //Console.WriteLine($"Original String: {fullName} ");//Amr Ramadan
+            //Console.WriteLine($"Substring: {subName}");//Ramadan
+
+
+            #endregion
+
+
+            #region Q5
+
+            //int x;
+            //int y;
+
+            //x = 10;
+            //y = x;
+
+            //Console.WriteLine(x);//10
+            //Console.WriteLine(y);//10
+
+            //Console.WriteLine("After Modifying X ");
+            //x = 20;
+
+            //Console.WriteLine(x);//20
+            //Console.WriteLine(y);//10
+
+            //Y Still 10 Because It Takes Copy Of X
+
+            #endregion
+
+
+            #region Q6
+
+            //Person person1 = new Person();
+            //person1.Name = "Amr";
+
+            //Console.WriteLine($"Person1 Name : {person1.Name}");//Amr
+
+            //// Assign Person1 To Person2
+            //Person person2 = person1;
+
+
+            ////Modify Person2
+            //person2.Name = "Ramadan";
+
+
+            //Console.WriteLine("After Assign Person1 To Person2 And Modify");
+            //Console.WriteLine($"Person1 Name : {person1.Name}");//Ramadan
+            //Console.WriteLine($"Person2 Name : {person2.Name}");//Ramadan
+
+            ////Both Refer(as pointer) To The Same Object In Memory
+
+
+
+
+
+
+
+            #endregion
+
+
+            #region Q7
+
+            //Console.WriteLine("Ente The First Sting: ");
+            //string first = Console.ReadLine();
+
+            //Console.WriteLine("Enter The Second String: ");
+            //string second = Console.ReadLine();
+
+            //string combined = first + second;
+
+            //Console.WriteLine($"After Combined String:{combined}");
+
+            #endregion
+
+
+            //MCQ
+
+            #region Q8
+
+            //Answer -> b) A value 1 will be assigned to d.
+
+            #endregion
+
+
+            #region Q9
+
+            //Answer -> d) 6 1
+
+            #endregion
+
+
+            #region Q10
+
+            //Answer -> d) 7 7
+
+            #endregion
+
+
+
+
+        }
+    }
+}
---
+++ Assignment 03/Program.cs
@@ -0,0 +1,136 @@
+﻿namespace Assignment_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Q1
+
+            //Console.WriteLine("Enter The Number : ");
+            //int num = int.Parse(Console.ReadLine());
+
+            //if (num % 3 == 0 && num % 4 == 0)
+            //Console.WriteLine("Yes");
+
+            //else
+            //    Console.WriteLine("No");
+
+
+            #endregion
+
+
+            #region Q2
+
+            //Console.WriteLine("Enter The Number : ");
+            //int num = int.Parse(Console.ReadLine());
+
+            //if (num < 0)
+            //{
+            //    Console.WriteLine("The Number Is Negative.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The Number Is Positive.");
+            //}
+            #endregion
+
+
+            #region Q3
+
+
+            //Console.WriteLine("Enter The First Number: ");
+            //int num1 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter The Second Number: ");
+            //int num2 = int.Parse(Console.ReadLine());
+
+
+            //Console.WriteLine("Enter The Third Number: ");
+            //int num3 = int.Parse(Console.ReadLine());
+
+            //int max = num1;
+            //if (num2 >  max && num2 > num3)
+            //    max = num2;
+            //else if(num3 > max && num3 > num2)
+            //    max = num3;
+
+            //int min = num1;
+            //if(num2 < min && num2 < num3)
+            //    min = num2;
+            //else if (num3 < min && num3 < num2)
+            //    min = num3;
+
+            //Console.WriteLine("Maximum number is: " + max);
+            //Console.WriteLine("Minimum number is: " + min);
+
+
+
+
+
+            #endregion
+
+
+            #region Q4
+
+            //Console.WriteLine("Enter The Number: ");
+            //int num = int.Parse(Console.ReadLine());
+
+            //if (num % 2 == 0)
+            //{
+            //    Console.WriteLine("The Number Is Even Number.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The Number Is Odd Number. ");
+            //}
+
+            #endregion
+
+
+            #region Q5
+
+            //Console.WriteLine("Enter The Character : ");
+            //char ch = char.Parse(Console.ReadLine());
+
+            //switch (ch)
+            //{
+
+            //    case 'a':        
+            //    case 'e':
+            //    case 'i':
+            //    case 'o':
+            //    case 'u':
+            //        Console.WriteLine("Vowel");
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("Consonant");
+            //        break;
+
+            //}
+
+
+            ////another solution with if
+
+            //Console.WriteLine("Enter a character: ");
+            //char ch = char.Parse(Console.ReadLine());
+
+
+            //if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+            //{
+            //    Console.WriteLine("vowel");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("consonant");
+            //}
+
+
+            #endregion
+
+
+
+        }
+    }
+}
