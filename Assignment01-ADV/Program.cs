75f2b489bbd24da2da448040e767ce3776ff3d12
amr <amrr.am7577@gamil.com>
2025-09-02 19:16:56 +0300

    Add Assignment-ADV01

---
+++ Assignment01-ADV/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Assignment01_ADV
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1
+
+            //int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
+
+            //Console.WriteLine("Original Array: " + string.Join(", ", arr));
+
+            //OptimizedBubbleSort.BubbleSort(arr);
+
+            //Console.WriteLine("Sorted Array: " + string.Join(", ", arr));
+
+            #endregion
+
+            #region Q2
+
+            //var intRange = new Range<int>(10, 1);
+            //Console.WriteLine(intRange.Length());
+            //Console.WriteLine(intRange.IsInRange(5)); 
+            //Console.WriteLine(intRange.IsInRange(11));
+
+            //var doubleRange = new Range<decimal>(9.8m,5.5m);
+            //Console.WriteLine(doubleRange.Length()); 
+            //Console.WriteLine(doubleRange.IsInRange(7.7m)); 
+            //Console.WriteLine(doubleRange.IsInRange(10.0m)); 
+
+            #endregion
+
+        }
+    }
+}
---
+++ Assignment01-ADV/Range.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Assignment01_ADV
+{
+    internal class Range<T> where T : IComparable<T> ,INumber<T>
+    {
+        public T Max {  get; set; }
+
+        public T Min { get; set; }
+        public Range(T max, T min) 
+        { 
+            Max = max;
+            Min = min;
+        }
+        public bool IsInRange(T Value) 
+        {
+            return Value.CompareTo(Min) >= 0 && Value.CompareTo(Max) <= 0;
+        }
+
+        public T Length() 
+        {
+            return T.Abs( Max - Min );
+        }
+
+        public override string ToString()
+        {
+            return $"[{Max} - {Min}]";
+        }
+    }
+}
