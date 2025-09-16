960987a41ddde8d4bf9097c9684ac88e4fd55014
amr <amrr.am7577@gamil.com>
2025-09-16 22:29:34 +0300

    Add Assignment-ADV03 work

---
+++ Assignment03-ADV03/LibraryEngine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment03_ADV03
+{
+    public class LibraryEngine
+    {
+        public delegate string BookDelegate(Book b);
+
+        public static void ProcessBooks(List<Book> bList, Func<Book,string> fPtr)
+        {
+            foreach (Book B in bList)
+            {
+                Console.WriteLine(fPtr(B));
+            }
+        }
+    }
+}
---
+++ Assignment03-ADV03/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Assignment03_ADV03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Book> books = new List<Book>()
+        {
+            new Book("111", "C# Basics", new string[]{"John", "Jane"}, new DateTime(2020,5,1), 300),
+            new Book("222", "Advanced C#", new string[]{"Alice"}, new DateTime(2022,10,15), 450)
+        };
+
+            Console.WriteLine("=== User Defined Delegate ===");
+            LibraryEngine.ProcessBooks(books, BookFunctions.GetTitle);
+
+            Console.WriteLine("\n=== BCL Delegate (Func) ===");
+            LibraryEngine.ProcessBooks(books, new Func<Book, string>(BookFunctions.GetAuthors));
+
+            Console.WriteLine("\n=== Anonymous Method (GetISBN) ===");
+            LibraryEngine.ProcessBooks(books, delegate (Book b) { return b.ISBN; });
+
+            Console.WriteLine("\n=== Lambda Expression (GetPublicationDate) ===");
+            LibraryEngine.ProcessBooks(books, b => b.PublicationDate.ToShortDateString());
+        }
+    }
+}
